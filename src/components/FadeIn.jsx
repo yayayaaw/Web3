be3c43cb26c5fd66b423e9ed7bfea2deeb6fdc39
Vynixid <karmayuga33@gmail.com>
2026-09-03 00:36:44 +0700

    Create FadeIn.jsx

---
+++ src/components/FadeIn.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { useScrollReveal } from '../hooks/useScrollReveal';
+
+const FadeIn = ({ children, delay = 0, className = '' }) => {
+  const { ref, isVisible } = useScrollReveal();
+  return (
+    <div
+      ref={ref}
+      className={`transition-all duration-1000 ease-out ${
+        isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'
+      } ${className}`}
+      style={{ transitionDelay: `${delay}ms` }}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default FadeIn;
